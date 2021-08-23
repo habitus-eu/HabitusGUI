14ce7f56d52c205616cdd40e2ac91e9d98129864
Vincent van Hees <vincentvanhees@gmail.com>
2021-08-23 12:06:32 +0200

    Adding first unit test #1

--- R/create_test_files.R
+++ R/create_test_files.R
@@ -12,8 +12,12 @@ create_test_files = function(dir=c(), Nfiles=10, Nobs = 10) {
   new_file_names = paste0(sample(x = 1:Nfiles, size = Nfiles, replace = FALSE)," recording.csv")
   for (i in 1:length(new_file_names)) {
     print(i)
-    data = data.frame(x = rnorm(mean = 1, sd = 1, n = Nobs),
-                      y = runif(min = 0, max = 100, n = Nobs))
+    set.seed(300)
+    x = rnorm(mean = 1, sd = 1, n = Nobs)
+    set.seed(400)
+    y = runif(min = 0, max = 100, n = Nobs)
+    data = data.frame(x = x,
+                      y = y)
     data = round(data, digits=4)
     
     write.csv(x = data, file = paste0(dir,"/",new_file_names[i]))
---
+++ tests/testthat.R
@@ -0,0 +1,3 @@
+library("testthat")
+library("HabitusGUI")
+test_check("HabitusGUI")
