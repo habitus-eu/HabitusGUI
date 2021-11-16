541a61cbbbfbd75e008272dfae7f2720c393895c
Vincent van Hees <vincentvanhees@gmail.com>
2021-11-16 15:08:59 +0100

    expanding params check to cover integers, doubles and sets

--- R/check_params.R
+++ R/check_params.R
@@ -7,13 +7,15 @@
 check_params = function(params = c()) {
   N = nrow(params)
   blocked_params = data.frame(name = character(N), error = character(N))
-  numi = which(params$class == "num")
   rowNames = rownames(params)
+  
+  # Check parameters which value should be numeric and inside a continues range.
+  numi = which(params$class == "num_double" | params$class == "num_integer")
   cnt = 1
   if (length(numi) > 0) {
-    for (i in 1:length(numi)) {
+    for (i in numi) {
       try(expr = {
-        test_num = any(is.na(suppressWarnings(as.numeric(params$value[numi[i]]))))
+        test_num = any(is.na(suppressWarnings(as.numeric(params$value[i]))))
       }, silent = TRUE)
       if (test_num == TRUE) {
         blocked_params$name[cnt] = rowNames[i]
@@ -21,19 +23,39 @@ check_params = function(params = c()) {
         cnt = cnt + 1
       } 
       if (test_num == FALSE) {
-        num_value = as.numeric(params$value[numi[i]])
+        num_value = as.numeric(params$value[i])
+        if (params$class[i] == "num_integer") {
+          if (round(num_value) != num_value) {
+            blocked_params$name[cnt] = rowNames[i]
+            blocked_params$error[cnt] = "Number is not an integer"
+            cnt = cnt + 1
+          }
+        }
         # below code accounts for both scalar and vector numerics
         if (any(num_value < as.numeric(params$minimum[i]) | 
                 num_value > as.numeric(params$maximum[i]))) {
           blocked_params$name[cnt] = rowNames[i]
-          blocked_params$error[cnt] = "Value not within expected range"
+          blocked_params$error[cnt] = "Number is not within expected range"
           cnt = cnt + 1
         }
+      }
+    }
+  }
+  
+  # Check parameters which value should be part of a set character or numeric.
+  seti = which(params$class == "set")
+  if (length(seti) > 0) {
+    for (i in seti) {
+      if (params$value[i] %in% unlist(strsplit(params$set[i], ";")) ==  FALSE) {
+        blocked_params$name[cnt] = rowNames[i]
+        blocked_params$error[cnt] = "Value is not part of expected set"
+        cnt = cnt + 1
       }
     }
   }
   if (cnt <= N) {
     blocked_params = blocked_params[-c(cnt:N),]
   }
+  block_params = blocked_params[order(blocked_params$name),]
   return(blocked_params)
 }
--- tests/testthat/test_check_params.R
+++ tests/testthat/test_check_params.R
@@ -1,18 +1,23 @@
 library(HabitusGUI)
 context("Check parameters during loading and before saving")
 test_that("Parameters are checked", {
-  params = data.frame(value = c("0", "1", "A", "", "NA"),
-                      topic = rep("topic", 5),
-                      description = rep("description", 5),
-                      class = rep("num", 5),
-                      minimum = rep("1", 5),
-                      maximum = rep("10", 5))
-  rownames(params) = c("a", "b", "c", "d", "e")
+  
+  params = data.frame(value = c("0", "1", "1.1", "A", "A", "", "NA"),
+                      topic = rep("topic", 7),
+                      description = rep("description", 7),
+                      class = c("num_double", "num_double", "num_integer", 
+                                "set", "set", "num_double", "num_double"),
+                      minimum = rep("1", 7),
+                      maximum = rep("10", 7),
+                      set = c(NA, NA, NA, "A;B;C", "D;E;F", NA, NA))
+  rownames(params) = c("a", "b", "c", "d", "e", "f", "g")
   paramcheck = check_params(params)
+  
   # Check that values are as expected
-  expect_equal(paramcheck$name, c("a", "c", "d", "e"))
-  expect_equal(paramcheck$error, c("Value not within expected range",
+  expect_equal(paramcheck$name, c("a", "c", "f", "g", "e"))
+  expect_equal(paramcheck$error, c("Number is not within expected range", 
+                                   "Number is not an integer",
                                    "Value is not numeric",
-                                   "Value is not numeric",
-                                   "Value is not numeric"))
+                                   "Value is not numeric", 
+                                   "Value is not part of expected set"))
 })
