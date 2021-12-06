28768df3e03a9474e905545399f96f35fed04050
Vincent van Hees <v.vanhees@accelting.com>
2021-12-06 18:28:55 +0100

    simplify PALMSpy command

--- R/PALMSpy_R.R
+++ R/PALMSpy_R.R
@@ -11,18 +11,11 @@ PALMSpy_R = function(gps_path, acc_path, output_path, config_file =c()) {
   # Note command assumes that PALMSPy is directly available from command line
   # If you installed PALMSpy in conda environment, make sure you run the HabitusGUI from that conda environment
   
-  # basecommand = paste0("/home/vincent/miniconda3/bin/conda run -n palmspy palmspy --gps-path ", gps_path, " --acc-path ", acc_path)
-  basecommand = paste0("palmspy --gps-path ", gps_path, " --acc-path ", acc_path)
+  basecommand = paste0("/home/vincent/miniconda3/bin/conda run -n palmspy palmspy --gps-path ", gps_path, " --acc-path ", acc_path)
+  # basecommand = paste0("palmspy --gps-path ", gps_path, " --acc-path ", acc_path)
   if (length(config_file) > 0) {
-    print(paste0(basecommand, " --config-file ",config_file))
     system(paste0(basecommand, " --config-file ",config_file))
   } else {
     system(basecommand)
-  }
-  if (!dir.exists("./PALMSpy_output")) {
-    warning("Folder PALMSpy_output not found")
-  } else {
-    file.copy(from = "./PALMSpy_output", to = output_path, 
-              overwrite = TRUE, recursive = TRUE, copy.mode = TRUE)
   }
 }
