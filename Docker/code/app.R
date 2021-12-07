c781455834935fd53109da36e391adb50e1eabfe
Vincent van Hees <v.vanhees@accelting.com>
2021-12-07 14:25:56 +0100

    changing working directory now both in Dockerfile and in app.R files before calling app

--- Docker/code/app.R
+++ Docker/code/app.R
@@ -1,8 +1,8 @@
 library("HabitusGUI")
-# if ("data" %in% dir()) {
-#   homedir = paste0(getwd(),"/data") # cd to data directory
-# } else {
-#   homedir = getwd()
-# }
-# HabitusGUI::myApp(homedir)
-HabitusGUI::myApp()
+if ("data" %in% dir()) {
+  homedir = paste0(getwd(),"/data") # cd to data directory
+} else {
+  homedir = getwd()
+}
+setwd(homedir)
+HabitusGUI::myApp(homedir)
