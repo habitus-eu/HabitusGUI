22650acb974179d2267c555d7f711a528c2ce50c
Vincent van Hees <v.vanhees@accelting.com>
2022-01-31 16:07:56 +0100

    edits to Ucloud bash script

--- UCloud_files/ucloud_HabitusGUI_installation.sh
+++ UCloud_files/ucloud_HabitusGUI_installation.sh
@@ -1,9 +1,25 @@
+# Install conda
+
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b
+source ~/.bashrc
+
+# now update conda and install pip
+conda update conda
+
 # Python / PALMSpy dependencies
 conda create -n palmspy python=3.7 openjdk=8.0 make=4.2.1 -y
 conda activate palmspy
 make
 make install
 make clean
+
+
+# Create symoblic link to be able to access all data in work work
+sudo mkdir /data
+sudo chown -R ucloud:ucloud /data
+ln -s /work/* /data/
 
 # R / HabitusGUI dependencies
 sudo apt-get update && apt-get install -y \
