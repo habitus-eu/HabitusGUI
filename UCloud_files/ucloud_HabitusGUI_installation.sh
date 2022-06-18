8e6fc4add88668ef1a83731de70a55401c397caf
Vincent van Hees <v.vanhees@accelting.com>
2022-06-18 16:15:45 +0200

    updating path and adding palmsplusr installation to code coverage test workflow

--- UCloud_files/ucloud_HabitusGUI_installation.sh
+++ UCloud_files/ucloud_HabitusGUI_installation.sh
@@ -29,7 +29,7 @@ echo "conda activate palmspy" >> ~/.bashrc
 # Create symoblic link to be able to access all data in work 
 sudo mkdir /srv/shiny-server/data
 sudo chown -R ucloud:ucloud /srv/shiny-server/data
-ln -s "/work/Member Files: LineMatthiesen#8897/*"  /srv/shiny-server/data/
+ln -s "/work/shared_test/*"  /srv/shiny-server/data/
 
 # R / HabitusGUI dependencies
 sudo apt-get update && sudo apt-get install -y \
@@ -42,7 +42,7 @@ R -e 'install.packages(c("shinyFiles", "shiny", "GGIR", "jsonlite", "DT", "waite
 R -e 'install.packages(c("activityCounts", "remotes", "shinyjs", "dplyr", "magrittr"), repos = "https://packagemanager.rstudio.com/cran/__linux__/focal/latest", dependencies = TRUE)'
 R -e 'install.packages(c("sf", "readr", "tidyr", "stringr", "sp", "raster", "lwgeom", "tidyverse"), repos = "https://packagemanager.rstudio.com/cran/__linux__/focal/latest", dependencies = TRUE)'
 R -e 'remotes::install_github("rstudio/bslib", dependencies = TRUE)' # development version because CRAN version has bug that affects us
-R -e 'remotes::install_github("vincentvanhees/palmsplusr", ref = "config_file", dependencies = TRUE)'
+R -e 'remotes::install_github("vincentvanhees/palmsplusr", dependencies = TRUE)'
 R -e 'remotes::install_github("habitus-eu/HabitusGUI", dependencies = TRUE)'
 
 
