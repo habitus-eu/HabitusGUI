82267a480a3283c663f8b54b5b08631b7b9269fc
Emiliano Molinaro <emil.molinaro@gmail.com>
2022-09-07 12:58:57 +0200

    run palmspy in background

--- R/PALMSpyshiny.R
+++ R/PALMSpyshiny.R
@@ -9,7 +9,7 @@
 PALMSpyshiny = function(outputdir, gpsdir, count_file_location) {
   basecommand = paste0("cd ",outputdir," ; nohup palmspy --gps-path ", gpsdir,
                        " --acc-path ", count_file_location,
-                       " --config-file ", paste0(outputdir, "/config.json"), " > /work/PALMSpy.log 2>&1")
+                       " --config-file ", paste0(outputdir, "/config.json"), " > /work/PALMSpy.log 2>&1 &")
   system2(command = "cd", args = gsub(pattern = "cd ", replacement = "", x = basecommand),
           stdout = "", stderr = "", wait = TRUE)
   return()
